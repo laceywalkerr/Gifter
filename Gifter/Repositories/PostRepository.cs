ef95dbe0abd529795ab350785ca1f16b0764699f
Lacey Walker <bluelarksong@gmail.com>
2020-09-30 15:15:07 -0500

    didnt save work, trying again.

--- Gifter/Repositories/PostRepository.cs
+++ Gifter/Repositories/PostRepository.cs
@@ -170,17 +170,17 @@ namespace Gifter.Repositories
                 using (var cmd = conn.CreateCommand())
                 {
                     cmd.CommandText = @"
-                SELECT p.Id AS PostId, p.Title, p.Caption, p.DateCreated AS PostDateCreated,
-                       p.ImageUrl AS PostImageUrl, p.UserProfileId AS PostUserProfileId,
+                        SELECT p.Id AS PostId, p.Title, p.Caption, up.DateCreated AS PostDateCreated,
+                               p.ImageUrl AS PostImageUrl, p.UserProfileId AS PostUserProfileId,
 
-                       up.Name, up.Bio, up.Email, p.DateCreated AS UserProfileDateCreated,
-                       up.ImageUrl AS UserProfileImageUrl,
+                               up.Name, up.Bio, up.Email, up.DateCreated AS UserProfileDateCreated,
+                               up.ImageUrl AS UserProfileImageUrl,
 
-                       c.Id AS CommentId, c.Message, c.UserProfileId AS CommentUserProfileId
-                  FROM Post p
-                       LEFT JOIN UserProfile up ON p.UserProfileId = up.id
-                       LEFT JOIN Comment c on c.PostId = p.id
-              ORDER BY p.DateCreated";
+                               c.Id AS CommentId, c.Message, c.UserProfileId AS CommentUserProfileId
+                          FROM Post p
+                               LEFT JOIN UserProfile up ON p.UserProfileId = up.id
+                               LEFT JOIN Comment c on c.PostId = p.id
+                      ORDER BY p.DateCreated";
 
                     var reader = cmd.ExecuteReader();
 
